4a2abde10428da0220b9f7ab481551b8a828eb15
Jacques Naude <JacquesN@Phumelela.com>
2021-12-18 19:47:52 +0200

    Add project files.

---
+++ SsmsObjectExplorer2.AddIn/ObjectExplorerToolWindow.cs
@@ -0,0 +1,34 @@
+﻿namespace SsmsObjectExplorer2.AddIn
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("ec4c0130-9b03-422f-9ec4-2ac0119beff3")]
+    public class ObjectExplorerToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectExplorerToolWindow"/> class.
+        /// </summary>
+        public ObjectExplorerToolWindow() : base(null)
+        {
+            this.Caption = "Object Explorer 2";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new ObjectExplorerToolWindowControl();
+        }
+    }
+}
